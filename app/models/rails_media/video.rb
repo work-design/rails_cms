186ce493603d777b2f727117d1f8a5141f016ee6
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-05 10:54:31 +0800

    default linked

--- app/models/rails_media/video.rb
+++ app/models/rails_media/video.rb
@@ -38,8 +38,6 @@ class Video < ApplicationRecord
       self.save!
       to_notification(
         receiver: self.author,
-        linked: self,
-        link: url_helpers.admin_videos_url(id: self.id),
         verbose: true
       ) if self.author
     end
