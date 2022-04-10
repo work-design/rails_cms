209cde46aecbbbc8f5978d3d01144cff282b0ab2
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-10 15:48:34 +0800

    deflt positon for carousel

--- app/models/cms/model/carousel.rb
+++ app/models/cms/model/carousel.rb
@@ -5,12 +5,17 @@ module Cms
     included do
       attribute :title, :string
       attribute :position, :integer
+      attribute :link_controller, :string
+      attribute :link_action, :string
+      attribute :link_params, :json
 
       has_one_attached :image
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
       acts_as_list scope: :organ_id
+
+      default_scope -> { order(position: :asc) }
     end
 
   end
