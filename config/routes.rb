d1cd03be4490b9cea80fc2c249a020760ed03943
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-24 20:44:49 +0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
-
-  namespace :cms, defaults: { business: 'cms' } do
-    resources :videos do
-      collection do
-        get :list
-        get :starred
-      end
-      member do
-        patch :viewed
-      end
-    end
-    resources :video_taxons
-    resources :video_tags
-    resources :audios, only: [:index]
-    resources :carousels
-
-    namespace :admin, defaults: { namespace: 'admin' } do
-      resources :videos
-      resources :audios
-      resources :carousels do
+  scope RailsExtend.default_routes_scope do
+    namespace :cms, defaults: { business: 'cms' } do
+      resources :videos do
+        collection do
+          get :list
+          get :starred
+        end
         member do
-          patch :reorder
+          patch :viewed
+        end
+      end
+      resources :video_taxons
+      resources :video_tags
+      resources :audios, only: [:index]
+      resources :carousels
+
+      namespace :admin, defaults: { namespace: 'admin' } do
+        resources :videos
+        resources :audios
+        resources :carousels do
+          member do
+            patch :reorder
+          end
         end
       end
     end
   end
-
 end
