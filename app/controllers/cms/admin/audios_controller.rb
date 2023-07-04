1f008101910ee109611d4d9d9eab92dccbcf58ef
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-04 12:03:39 +0800

    show more jinfos

--- app/controllers/cms/admin/audios_controller.rb
+++ app/controllers/cms/admin/audios_controller.rb
@@ -3,7 +3,10 @@ module Cms
     before_action :set_audio, only: [:show, :edit, :update, :destroy]
 
     def index
-      @audios = Audio.page(params[:page])
+      q_params = {}
+      q_params.merge! default_params
+
+      @audios = Audio.includes(media_attachment: :blob).default_where(q_params).page(params[:page])
     end
 
     private
@@ -12,12 +15,13 @@ module Cms
     end
 
     def audio_params
-      params.fetch(:audio, {}).permit(
+      p = params.fetch(:audio, {}).permit(
         :title,
         :state,
         :author_id,
         :media
       )
+      p.merge! default_form_params
     end
 
   end
