e32e9506c5057482351886e4a57f22db88f7f9b1
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-16 00:25:24 +0800

    gem

--- app/models/cms/video.rb
+++ app/models/cms/video.rb
@@ -4,8 +4,8 @@ module Cms
     #include CheckMachine
     include Growth::Ext::Entity if defined? RailsGrowth
     if defined? RailsInteract
-      include Interact::Model::Like
-      include Interact::Model::Commentable
+      include Interact::Ext::Like
+      include Interact::Ext::Commentable
     end
   end
 end
