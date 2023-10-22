56313abeb78305413dae69f82c606d8ad6c89d5b
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-22 10:46:30 +0800

    tag curd

--- app/controllers/cms/admin/tags_controller.rb
+++ app/controllers/cms/admin/tags_controller.rb
@@ -1,3 +1,13 @@
-class Cms::Admin::TagsController < Cms::Admin::BaseController
+module Cms
+  class Admin::TagsController < Admin::BaseController
+    def index
+      q_params = {}
+      q_params.merge! default_params
 
+      @tags = Tag.default_where(q_params).order(id: :asc).page(params[:page])
+    end
+
+
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
       namespace :admin, defaults: { namespace: 'admin' } do
         root 'home#index'
+        resources :tags
         resources :covers
         resources :videos
         resources :audios
