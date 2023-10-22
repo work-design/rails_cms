0c8e5b01c919a8e4231020979d384fa2d5592ed2
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-22 10:56:41 +0800

    muilt select tg id

--- app/controllers/cms/admin/audios_controller.rb
+++ app/controllers/cms/admin/audios_controller.rb
@@ -1,17 +1,22 @@
 module Cms
   class Admin::AudiosController < Admin::BaseController
     before_action :set_audio, only: [:show, :edit, :update, :destroy]
+    before_action :set_tags, only: [:new, :edit]
 
     def index
       q_params = {}
       q_params.merge! default_params
 
-      @audios = Audio.includes(media_attachment: :blob).default_where(q_params).page(params[:page])
+      @audios = Audio.includes(audio_tags: :tag, media_attachment: :blob).default_where(q_params).page(params[:page])
     end
 
     private
     def set_audio
       @audio = Audio.find(params[:id])
+    end
+
+    def set_tags
+      @tags = Tag.default_where(default_params)
     end
 
     def audio_params
@@ -20,7 +25,8 @@ module Cms
         :state,
         :author_id,
         :media,
-        :cover
+        :cover,
+        tag_ids: []
       )
       p.merge! default_form_params
     end
